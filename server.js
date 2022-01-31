2cfef7cb34871d3d751fc125fa4fb24dfa47c6b6
Keller Lawson <keeperkell@gmail.com>
2022-01-30 23:02:17 -0800

    updating server.js

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var express = require('express')
 var app = express();
 
 var corsOptions = {
-  origin: 'https://main.dk7q11eybhlt4.amplifyapp.com/',
-  optionsSuccessStatus: 200 
+  origin: "http://localhost:3000"
 };
 
 var orderID = 1;
@@ -22,6 +21,13 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  next();
+});
+
 // simple route
 app.get("/", (req, res) => {
   res.json({ message: "OnlyFactories BD Port!" });
@@ -29,24 +35,24 @@ app.get("/", (req, res) => {
 
 //Query Start for finding an Order by orderID. Sent 
 //from localhost:3000/tracking to localhost:3306/tracking
-app.post('/tracking', (req, res) => {
-  var{orderID} = req.body;
-  var records = [[req.body.orderID]];
-  if(records[0][0]!=null){
-    connection.query(`SELECT * from FactoryOrders WHERE orderID = ${orderID}`, function(err,res,fields){
+app.get('/api/tracking/:id', (req, res) => {
+  const orderID = req.params.id;
+
+  if(orderID != null){
+    connection.query(`SELECT * from FactoryOrders WHERE orderID = ${orderID}`, function(err,results,fields){
       if(err) throw err;
 
-      console.log(res);
+      console.log(results);
+      res.json(results);
+      return;
     });
   }
-  res.json('Order found');
-  res.json(records);
 })
 //Query END
 
 //Query Start for creating an order. Sent from 
 //localhost:3000/ordering to localhost:3306/ordering
-app.post('/ordering', (req, res) => {
+app.post('/api/ordering', (req, res) => {
   //var{orderID} = 2
   var{orderID, fullName, email, quantityRED, quantityBLUE, quantityWHITE, orderStatus, transactionID, created_at, updated_at} = req.body;
   /*var{email} = req.body.Email;
@@ -74,10 +80,11 @@ app.post('/ordering', (req, res) => {
 require("../NodeJS/routes/orders.routes.js")(app);
 
 // set port, listen for requests
+const PORT = process.env.PORT || 3306;
 https.createServer({
 	key: fs.readFileSync("/etc/letsencrypt/live/onlyfactories.duckdns.org/privkey.pem"),
 	cert: fs.readFileSync("/etc/letsencrypt/live/onlyfactories.duckdns.org/cert.pem"),
 	},
-	app).listen(3306, function(){
+	app).listen(PORT, function(){
 		console.log("Running on port 3306");
 });
