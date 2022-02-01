732b4c74d74eae678818393b8e109a60e5391462
Keller Lawson <keeperkell@gmail.com>
2022-01-31 23:18:02 -0800

    added getMaxOrderID API call

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.get("/", (req, res) => {
   res.json({ message: "OnlyFactories BD Port!" });
 });
 
-//Query Start for finding an Order by orderID. Sent 
-//from localhost:3000/tracking to localhost:3306/tracking
+//Query Start for finding an Order by orderID.
 app.get('/api/tracking/:id', (req, res) => {
   const orderID = req.params.id;
 
@@ -44,33 +43,49 @@ app.get('/api/tracking/:id', (req, res) => {
 })
 //Query END
 
-//Query Start for creating an order. Sent from 
-//localhost:3000/ordering to localhost:3306/ordering
-app.post('/api/ordering', (req, res) => {
-  //var{orderID} = 2
-  var{orderID, fullName, email, quantityRED, quantityBLUE, quantityWHITE, orderStatus, transactionID, created_at, updated_at} = req.body;
-  /*var{email} = req.body.Email;
-  var{quantityRED} = req.body.quantityRED;
-  var{quantityBLUE} = req.body.quantityBLUE;
-  var{quantityWHITE} = req.body.quantityWHITE;
-  var{orderStatus} = "Created"
-  var{transactionID} = 222222;
-  var{created_at} = new Date().getTime();
-  var{updated_at} = new Date().getTime();*/
+//Query Start for finding MAX orderID.  
+app.get('/api/getMaxOrderID', (req, res) => {
 
+  if(orderID != null){
+    connection.query(`SELECT MAX(orderID) from FactoryOrders`, function(err,results,fields){
+      if(err) throw err;
+
+      console.log(results);
+      res.json(results);
+      return;
+    });
+  }
+})
+//Query END
+
+//Query Start for creating an order. 
+app.post('/api/ordering', (req, res) => {
+  
+  //set keys/map for request body
+  var{orderID, fullName, email, quantityRED, quantityBLUE, quantityWHITE, orderStatus, transactionID, created_at, updated_at} = req.body;
+
+  //define map and set to records array
   var records = [[req.body.orderID, req.body.fullName, req.body.email, req.body.orderStatus, req.body.transactionID, req.body.quantityRED, req.body.quantityBLUE, req.body.quantityWHITE, req.body.created_at, req.body.updated_at]];
+  
+  //check if submitted request was empty
+  //query database if not empty
   if(records[0][0]!=null){
     connection.query("INSERT INTO FactoryOrders (orderID, fullName, email, orderStatus, transactionID, quantityRED, quantityBLUE, quantityWHITE, created_at, updated_at) VALUES ?", [records], function(err,res,fields){
+      
+      //if there is an error querying database, throw error
       if(err) throw err;
 
       console.log(res);
     });
   }
+  //return message and data received from query
   res.json('Order received');
   res.json(records);
 })
 //Query END
 
+//probably move all routing queries to this location in the future, 
+//as API expands
 require("../NodeJS/routes/orders.routes.js")(app);
 
 // set port, listen for requests
